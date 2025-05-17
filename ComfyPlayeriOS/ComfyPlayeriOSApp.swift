7f9b576c416bac782ce226fbcf3eeb0872a5488b
AryanRogye <aryan.rogye@gmail.com>
2025-05-17 02:07:32 -0500

    added iOS Target

---
+++ ComfyPlayeriOS/ComfyPlayeriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComfyPlayeriOSApp.swift
+//  ComfyPlayeriOS
+//
+//  Created by Aryan Rogye on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ComfyPlayeriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
